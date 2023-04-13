f0e258784977cb634501c34ae48167677932710a
Jackson Hoffart <jackson.hoffart@gmail.com>
2023-04-13 10:10:14 +0200

    add script of dependencies

---
+++ main.R
@@ -0,0 +1,8 @@
+library(dplyr) # a CRAN package 
+library(pacta.data.scraping) # a public GITHUB package
+library(pacta.data.preparation) # a private GITHUB package
+library(rlang) # a CRAN package, but later we will enforce that it is a certain version
+
+if (packageVersion("rlang") != "1.0.6") {
+  stop("Please install rlang version `1.0.6`")
+}
