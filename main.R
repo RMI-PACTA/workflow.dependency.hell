b75758f6ecb76812c33ccd3686513e8ec4efcd31
CJ Yetman <cj@cjyetman.com>
2023-04-14 11:10:36 +0200

    CRAN package but need the dev GitHub version

--- main.R
+++ main.R
@@ -2,6 +2,7 @@ library(dplyr) # a CRAN package
 library(pacta.data.scraping) # a public GITHUB package
 library(pacta.data.preparation) # a private GITHUB package
 library(rlang) # a CRAN package, but later we will enforce that it is a certain version
+library(dbplyr) # a CRAN package, but we need the dev GitHub version
 
 if (packageVersion("rlang") != "1.0.6") {
   stop("Please install rlang version `1.0.6`")
